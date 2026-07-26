75a25bcc8bbfef7de7e26c0debac31e27c76a4fd
IshmanM <ishman.mann7@gmail.com>
2026-07-26 17:43:55 -0400

    trying hardware pwm for dc motor

--- src/endpoint/main.py
+++ src/endpoint/main.py
@@ -57,8 +57,8 @@ if __name__ == "__main__":
         servo_driver=servo_driver,
         dc_motor_driver=dc_motor_driver,
         foam_channel=config.FOAM_CHANNEL,
-        motor_1_speed=0.99,
-        motor_2_speed=0.99
+        motor_1_speed=0.5,
+        motor_2_speed=0.5
     )
     
     controller = EndpointController(orient_mechanism, foam_mechanism)
